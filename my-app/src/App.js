8705c4d1811bd27b8295f2b95b72780d9aabdb2d
Sang Hun Park <62207757+bp4sp4@users.noreply.github.com>
2023-08-15 23:21:43 +0900

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,7 +3,35 @@ import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
 function App() {
-  return <div></div>;
+  const [toDo, setTodo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setTodo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if(toDo === "") {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setTodo("");
+  };
+  console.log(toDos)
+  return <div>
+    <h1>My to Dos ({toDos.length})</h1>
+    <form onSubmit={onSubmit}>
+    <input 
+    onChange={onChange} 
+    value={toDo} 
+    type="text" 
+    placeholder="Write your to do"
+     />
+     <button>Add To Do</button>
+     </form>
+     <hr />
+     <ul>
+     {toDos.map((item, index) => (
+     <li key={index}>{item}</li> ))}
+     </ul>
+  </div>;
   
 }
 
